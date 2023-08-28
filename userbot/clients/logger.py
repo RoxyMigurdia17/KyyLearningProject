dc31d70f70be62a8787775b569bdc252c58252d3
ahmadaarifky17 <admin@my-kylia1704.my.id>
2023-08-28 10:32:14 +0700

    Update logger.py

--- userbot/clients/logger.py
+++ userbot/clients/logger.py
@@ -17,7 +17,7 @@ from userbot import MAN2, MAN3, MAN4, MAN5, bot, branch, tgbot
 from userbot.utils import checking
 
 MSG_ON = """
-🔥 **Man-Userbot Berhasil Di Aktifkan**
+🔥 **KyyLearningProject Berhasil Di Aktifkan**
 ━━
 ➠ **Userbot Version -** `{}@{}`
 ➠ **Ketik** `{}alive` **untuk Mengecheck Bot**
